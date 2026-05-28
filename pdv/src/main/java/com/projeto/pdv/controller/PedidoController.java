1fae182b902c9174a57de4f967b6648ef1fba45d
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-05-28 17:53:16 -0300

    pedido Controller

---
+++ pdv/src/main/java/com/projeto/pdv/controller/PedidoController.java
@@ -0,0 +1,59 @@
+package com.projeto.pdv.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projeto.pdv.dto.PedidoDTO;
+import com.projeto.pdv.service.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoController {
+    @Autowired
+    private PedidoService service;
+
+    @GetMapping
+    public ResponseEntity<List<PedidoDTO>> findAll(){
+        List<PedidoDTO> dto = service.findAll();
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PedidoDTO> findById(@PathVariable Long id){
+        PedidoDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoDTO> insert(@RequestBody PedidoDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PedidoDTO> update(@PathVariable Long id, @RequestBody PedidoDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<PedidoDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
