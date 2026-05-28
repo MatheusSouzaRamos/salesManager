b38a131fe78ff05dc4ea1350b8cf27e68c265325
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-05-28 17:30:33 -0300

    fix imports

--- pdv/src/main/java/com/projeto/pdv/controller/ClienteController.java
+++ pdv/src/main/java/com/projeto/pdv/controller/ClienteController.java
@@ -1,10 +1,8 @@
 package com.projeto.pdv.controller;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
