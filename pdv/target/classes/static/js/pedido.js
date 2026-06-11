3278b9a9ab085157909da694e5dc9e1131db03e6
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-06-11 17:02:55 -0300

    iniciando fetch pedido

---
+++ pdv/target/classes/static/js/pedido.js
@@ -0,0 +1,43 @@
+function buscarTodosPedidos(){
+    fetch("http://localhost:8080/pedidos", {
+        method: "GET",
+        headers: {
+            "Accept" : "application/json",
+            "Content-type" : "application/json"
+        }
+    })
+    .then(res => {
+        if(!res.ok) throw new Error("Erro ao consultar dados.");
+        return res.json();
+    })
+    .then(data => {
+        console.log("Dados: ", data);
+    })
+    .catch(erro => {
+        console.log("Erro: ", erro)
+    })
+}
+
+function buscarPedidoId(){
+    const idBuscar = document.getElementById("idbuscarpedido").value;
+
+    // if idBuscar vazio {......}
+
+    fetch(`http://localhost:8080/pedidos/${idBuscar}`, {
+        method: "GET",
+        headers: {
+            "Accept" : "application/json",
+            "Content-type" : "application/json"
+        }
+    })
+    .then(res => {
+        if(!res.ok) throw new Error("Erro ao consultar dados.");
+        return res.json();
+    })
+    .then(data => {
+        console.log("Dados: ", data);
+    })
+    .catch(erro => {
+        console.log("Erro: ", erro)
+    })
+}
