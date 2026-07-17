a6917f85ff2ac13c3a09f031d5a17b447828672e
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-07-17 16:21:37 -0300

    montando carrinho e lado lado

--- pdv/target/classes/static/js/carrinho.js
+++ pdv/target/classes/static/js/carrinho.js
@@ -1,133 +1,154 @@
-function buscarTodosProdutosCarrinho(){
-    fetch("http://localhost:8080/produtos", {
+async function buscarTodosProdutosCarrinho(){
+    const res = await fetch("http://localhost:8080/produtos", {
         method: "GET",
         headers: {
             "Accept" : "application/json",
             "Content-type" : "application/json"
         }
     })
-    .then(res => {
-        if(!res.ok) throw new Error("Erro ao consultar dados.");
-        return res.json();
-    })
-    .then(data => {
-        console.log("Dados: ", data);
-        let tabela = document.getElementById("tabelaProdutos");
 
-        let linhas = "";
-        for(const el of data){
-            linhas = linhas + `<tr>
-                <td>${el.id}</td>
-                <td>${el.nome}</td>
-                <td>R$ ${el.valor}</td>
-                <td><button style="margin: auto; display: block;" onclick="adicionarCarrinho(${el.id})">+</button></td>
-                
-            </tr>`
-        }
+    if(!res.ok){
+        throw new Error("Erro ao consultar dados.");
+    }
         
-        tabela.innerHTML = `
-        <h2>Produtos</h2>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Produto</th>
-                    <th>Valor</th>
-                    <th>Adicionar</th>
-                </tr>
-                ${linhas}
-            </table>
+    const data = await res.json();
+    console.log("Dados: ", data);
+    let tabela = document.getElementById("tabelaProdutos");
 
-        `;
+    let linhas = "";
+    for(const el of data){
+        linhas = linhas + `<tr>
+            <td>${el.id}</td>
+            <td>${el.nome}</td>
+            <td>R$ ${el.valor}</td>
+            <td><button style="margin: auto; display: block;" onclick="adicionarCarrinho(${el.id})">+</button></td>
+            
+        </tr>`
+    }
+    
+    tabela.innerHTML = `
+    <h2>Produtos</h2>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Produto</th>
+                <th>Valor</th>
+                <th>Adicionar</th>
+            </tr>
+            ${linhas}
+        </table>
 
-    })
-    .catch(erro => {
-        console.log("Erro: ", erro)
-    })
+    `;    
 }
 
-function listarCarrinho(){
-    fetch("http://localhost:8080/carrinho", {
+async function listarCarrinho(){
+    const res = await fetch("http://localhost:8080/carrinho", {
         method: "GET",
         headers: {
             "Accept": "application/json",
             "Content-type": "application/json"
         }
     })
-    .then(res => {
-        if(!res.ok) throw new Error("Erro ao listar carrinho.");
-        return res.json();
-    })
-    .then(data => {
-        console.log("Dados: ", data);
-        let tabela = document.getElementById("divCarrinho");
-        let estrutura = `
-            <h2>Carrinho</h2>
-            <table>
-                <tr>
-                    <th>Produto</th>
-                    <th>Valor</th>
-                    <th>Quantidade</th>
-                    <th>Remover</th>
-                </tr>
-            `
-        
-        let linhas = "";
-        for(let el of data){
-            linhas = linhas + `
+
+    if(!res.ok) {
+        throw new Error("Erro ao listar carrinho.");
+    }
+
+    const data = await res.json();
+    console.log("Dados: ", data);
+    let tabela = document.getElementById("divCarrinho");
+    let estrutura = `
+        <h2>Carrinho</h2>
+        <table>
             <tr>
-                <td>${el.produto.nome}</td>
-                <td>${el.produto.valor}</td>
-                <td>${el.quantidade}</td>
-                <td><button style="margin: auto; display: block;" onclick="removerCarrinho(${el.produto.id})">-</button></td>
-            </tr>`
-        }
-        tabela.innerHTML = `
-            ${estrutura}
-            ${linhas}
-            </table>
+                <th>Produto</th>
+                <th>Valor</th>
+                <th>Quantidade</th>
+                <th>Remover</th>
+            </tr>
         `
-    })
-    .catch(erro => {
-        console.log("Erro: ", erro);
-    })
+
+    let linhas = "";
+    for(let el of data){
+        linhas = linhas + `
+        <tr>
+            <td>${el.produto.nome}</td>
+            <td>${el.produto.valor}</td>
+            <td>${el.quantidade}</td>
+            <td><button style="margin: auto; display: block;" onclick="removerCarrinho(${el.produto.id})">-</button></td>
+        </tr>`
+    }
+    tabela.innerHTML = `
+        ${estrutura}
+        ${linhas}
+        </table>
+    `
+
+    totaisCarrinho();
 }
 
-function adicionarCarrinho(id){
+async function adicionarCarrinho(id){
     // let id = document.getElementById("idProdutoCarrinho").value;
     // let quantidade = document.getElementById("idQuantidadeCarrinho").value;
     let quantidade = 1;
 
-    fetch(`http://localhost:8080/carrinho/inserir/${id}/${quantidade}`, {
+    const res = await fetch(`http://localhost:8080/carrinho/inserir/${id}/${quantidade}`, {
         method: "POST",
         headers: {
             "Accept" : "application/json",
             "Content-type" : "application/json"
         }
     })
-    .then(res => {
-        if(!res.ok) throw new Error("Erro ao inserir no carrinho.");
-    })
-    .catch(erro => {
-        console.log("Erro: ", erro)
-    })
+    
+    if(!res.ok){
+        throw new Error("Erro ao inserir no carrinho.");
+    }
+
+    listarCarrinho();
+    totaisCarrinho();
 }
 
-function removerCarrinho(id){
+async function removerCarrinho(id){
     // let id = document.getElementById("idProdutoCarrinho").value;
     // let quantidade = document.getElementById("idQuantidadeCarrinho").value;
     let quantidade = -1;
 
-    fetch(`http://localhost:8080/carrinho/inserir/${id}/${quantidade}`, {
+    const res = await fetch(`http://localhost:8080/carrinho/inserir/${id}/${quantidade}`, {
         method: "POST",
         headers: {
             "Accept" : "application/json",
             "Content-type" : "application/json"
         }
     })
-    .then(res => {
-        if(!res.ok) throw new Error("Erro ao inserir no carrinho.");
+
+    if(!res.ok) {
+        throw new Error("Erro ao inserir no carrinho.");
+    }
+
+    listarCarrinho();
+    totaisCarrinho();
+}
+
+async function totaisCarrinho(){
+    const res = await fetch("http://localhost:8080/carrinho/totais", {
+        method: "GET",
+        headers : {
+            "Accept" : "application/json",
+            "Content-type" : "application/json"
+        }
     })
-    .catch(erro => {
-        console.log("Erro: ", erro)
-    })
+
+    if(!res.ok){
+        throw new Error("Erro ao buscar totais do carrinho");
+    }
+
+    data = await res.json();
+    console.log(data);
+
+    let totais = document.getElementById("totaisCarrinho");
+    totais.innerHTML = `
+        <p>Total R$ ${data[0]}   Total de Itens: ${data[1]}   Produtos: ${data[2]}</p>
+        
+        ` 
+    
 }
